c258b24e092a09261d90476c2337d344e68b00f7
jonathanlam10 <jonathanlam10@csu.fullerton.edu>
2023-02-07 18:11:16 -0800

    fixed functions

--- function.cpp
+++ function.cpp
@@ -2,11 +2,13 @@
 
 readFile() //function for reading from readme
 {
-    ofstream outfile; //open file to write in
-    getline(infile, name); //getting line from readme file
-    cout << name << endl; //displays line
-
-    outfile.close(); //close file
+    ifstream infile; //open file to write in
+    while(infile!eof) //loop to make sure the program goes through the whole file
+    {    
+        getline(infile, name); //getting line from readme file
+        cout << name << endl; //displays line
+    }
+    infile.close(); //close file
 }
 
 
