7e86cde06a047c4c078e102335a357d0d9e2594b
jonathanlam10 <jonathanlam10@csu.fullerton.edu>
2023-02-07 17:56:10 -0800

    finish adding functions

---
+++ function.cpp
@@ -0,0 +1,12 @@
+#include "functions.h"
+
+readFile() //function for reading from readme
+{
+    ofstream outfile; //open file to write in
+    getline(infile, name); //getting line from readme file
+    cout << name << endl; //displays line
+
+    outfile.close(); //close file
+}
+
+
