3a76f970924da2e5ab85d3188d287f1d63c4c900
Kevin <kchoum1@gmail.com>
2023-02-07 18:54:17 -0800

    se enter the commit message for your changes. Lines starting

---
+++ labmain.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <function.h>
+
+using namespace std;
+
+int main()
+{
+	ifstream f; //create file var
+	f.open(readme); //define var with file
+	function.readfile(f) //use function to read and display file
+}
